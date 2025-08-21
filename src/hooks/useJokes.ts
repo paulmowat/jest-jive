ac08ba8cfbca62ca75e1a7aa1967edecb2b4dbde
Paul Mowat <paul.mowat@oneadvanced.com>
2025-08-21 09:02:05 +0100

    feat: add joke

--- src/hooks/useJokes.ts
+++ src/hooks/useJokes.ts
@@ -36,7 +36,8 @@ const jokes = [
   "What do you call a bear with no teeth? A gummy bear!",
   "Knock knock. Who's there? Lettuce. Lettuce who? Lettuce in, it's cold out here!",
   "How do you comfort a JavaScript bug? You console it.",
-  "Why do programmers prefer dark mode? Because light attracts bugs!"
+  "Why do programmers prefer dark mode? Because light attracts bugs!",
+  "Why did the function return early? Because it had a date."
 ];
 
 export const useJokes = () => {
